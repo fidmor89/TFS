6283134331e0124b1e4cd88c5fb78f4386ba642d
Fidel Esteban Morales Cifuentes <fidmor.89@outlook.com>
2015-09-05 16:57:56 -0600

    Class for the API wrapper created

---
+++ TFS/VSORestAPIWrapper.swift
@@ -0,0 +1,16 @@
+//
+//  VSORestAPIWrapper.swift
+//  TFS
+//
+//  Created by Fidel Esteban Morales Cifuentes on 9/5/15.
+//  Copyright (c) 2015 Fidel Esteban Morales Cifuentes. All rights reserved.
+//
+
+import Foundation
+
+class APIWrapper {
+    
+    
+
+    
+}
