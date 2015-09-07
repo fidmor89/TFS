cc09c1f603c0e84d4d1629271b452f4750aab0f6
Fidel Esteban Morales Cifuentes <fidmor.89@outlook.com>
2015-09-06 18:43:24 -0600

    JSON parsing for teamProjects added under code 200 JSON parsed using ’SwiftyJSON', '~> 2.2.1'
    Podfile now contains:
    source 'https://github.com/CocoaPods/Specs.git'
    platform :ios, '8.3'
    use_frameworks!
    
    pod 'SwiftHTTP', '~> 0.9.4'
    pod ’SwiftyJSON', '~> 2.2.1'

--- TFS/VSORestAPIWrapper.swift
+++ TFS/VSORestAPIWrapper.swift
@@ -8,6 +8,7 @@
 
 import Foundation
 import SwiftHTTP
+import SwiftyJSON
 
 class APIWrapper {
     
@@ -35,12 +36,36 @@ class APIWrapper {
                 return //also notify app of failure as needed
             }
             if let data = response.responseObject as? NSData {
+                
                 let str = NSString(data: data, encoding: NSUTF8StringEncoding)
-                println("response: \(str)") //prints the HTML of the page
+                println("response: \(str)")
                 
                 
                 switch (response.statusCode!) {
                 case 200://Success, and there is a response body.
+                    
+                    let readableJSON = JSON(data:data,options:NSJSONReadingOptions.MutableContainers, error:nil)
+                    var count: Int = readableJSON["count"].int as Int!;
+                    var jsonOBJ = readableJSON["value"]
+
+                    var projectList = [teamProjects]()
+
+                    for index in 0...(count-1) {
+                        
+                        let id = jsonOBJ[index]["id"].string as String! ?? ""
+                        let name: String = jsonOBJ[index]["name"].string as String! ?? ""
+                        let desc: String = jsonOBJ[index]["description"].string as String! ?? ""
+                        let url: String = jsonOBJ[index]["url"].string as String! ?? ""
+                        let state: String = jsonOBJ[index]["state"].string as String! ?? ""
+                        let revision: Int = jsonOBJ[index]["revision"].int as Int! ?? 0
+                        
+                        let teamProject: teamProjects = teamProjects(id: id, name: name, description: desc, url: url, state: state, revision: revision)
+                        
+                        projectList.append(teamProject);
+                    }
+                    
+                    
+                    println(projectList)
                     
                     break;
                 case 201:   //Success, when creating resources. Some APIs return 200 when successfully creating a resource. Look at the docs for the API you're using to be sure.
@@ -76,11 +101,21 @@ class APIWrapper {
 }//class
 
 
-struct teamProjects {
+
+public struct teamProjects {
     var id: String = ""
     var name: String = ""
     var description: String = ""
     var url: String = ""
     var state: String = ""
     var revision: Int = 0
+    
+    init(id:String, name: String, description:String, url:String, state:String, revision: Int){
+        self.id = id
+        self.name = name
+        self.description = description
+        self.url = url
+        self.state = state
+        self.revision = revision
+    }
 }
