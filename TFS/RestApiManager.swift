d0f76d294c5a3876290c664558ebca92cbdc89cc
Fidel Esteban Morales Cifuentes <fidmor.89@outlook.com>
2015-09-20 17:39:14 -0600

    RestApiManager class created in blank

---
+++ TFS/RestApiManager.swift
@@ -0,0 +1,9 @@
+//
+//  RestApiManager.swift
+//  TFS
+//
+//  Created by Fidel Esteban Morales Cifuentes on 9/20/15.
+//  Copyright (c) 2015 Fidel Esteban Morales Cifuentes. All rights reserved.
+//
+
+import Foundation
