9f272c78ca77de6475f9facf77b44dbc86a53250
Fidel Esteban Morales Cifuentes <fidmor.89@outlook.com>
2015-09-20 18:07:54 -0600

    getTeamProjects and makeHTTPGetRequest functions implemented

--- TFS/RestApiManager.swift
+++ TFS/RestApiManager.swift
@@ -7,3 +7,32 @@
 //
 
 import Foundation
+import SwiftyJSON
+
+typealias ServiceResponse = (JSON, NSError?) -> Void
+
+class RestApiManager: NSObject {
+    static let sharedInstance = RestApiManager()            //To use manager class as a singleton.
+    
+    let baseURL = "https://almlatam.visualstudio.com"
+    
+    func getTeamProjects(usr: String, pw: String, onCompletion: (JSON) -> Void) {
+        let route = baseURL + "DefaultCollection/_apis/projects?api-version=2.0"            //API request route
+        
+        makeHTTPGetRequest(route, onCompletion: { json, err in
+            onCompletion(json as JSON)
+        })
+    }
+    
+    func makeHTTPGetRequest(path: String, onCompletion: ServiceResponse) {
+        let request = NSMutableURLRequest(URL: NSURL(string: path)!)
+        
+        let session = NSURLSession.sharedSession()
+        
+        let task = session.dataTaskWithRequest(request, completionHandler: {data, response, error -> Void in
+            let json:JSON = JSON(data: data)
+            onCompletion(json, error)
+        })
+        task.resume()
+    }
+}
