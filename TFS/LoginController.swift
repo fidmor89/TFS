bf8d79829b5009354a7e81ffbc4f6b2e30642643
Fidel Esteban Morales Cifuentes <fidmor.89@outlook.com>
2015-10-03 10:08:14 -0600

    LoginController class created and linked to the Login view in the Main Storyboard

---
+++ TFS/LoginController.swift
@@ -0,0 +1,26 @@
+//
+//  LoginController.swift
+//  TFS
+//
+//  Created by Fidel Esteban Morales Cifuentes on 10/3/15.
+//  Copyright (c) 2015 Fidel Esteban Morales Cifuentes. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class LoginController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
+
