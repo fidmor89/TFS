3352287913f3c7df7839842cdcf2512e0dd36c04
Fidel Esteban Morales Cifuentes <fidmor.89@outlook.com>
2015-10-03 22:23:50 -0600

    Logic to display Teams, Projects and collections in the menu added.

--- TFS/menuController.swift
+++ TFS/menuController.swift
@@ -9,37 +9,107 @@
 import Foundation
 
 class menuController: UITableViewController {
-
+    
+    var displayedMenu : String = "";
     var collections:[(id: String, name: String, url: String)] = []
-
+    var teams:[(id: String, name: String, url: String, description: String, identityUrl: String)] = []
+    var projects:[(id: String, name: String, description: String, url: String, state: String, revision: String)] = []
+    
+    
     override func viewWillAppear(animated: Bool) {
         super.viewWillAppear(animated)
         
-        //cal API and get project collections
-        RestApiManager.sharedInstance.getCollections { json in
-            var count: Int = json["count"].int as Int!;         //number of objects within json obj
-            var jsonOBJ = json["value"]                         //get json with projects
-            
-            for index in 0...(count-1) {                                                    //for each obj in jsonOBJ
+        if RestApiManager.sharedInstance.collection == nil
+        {
+            displayedMenu = "Collections"
+        }
+        
+//        displayedMenu = "Teams"
+//        RestApiManager.sharedInstance.collection = "DefaultCollection"
+//        RestApiManager.sharedInstance.projectId = "b5be0374-7dea-41bb-b5e7-47f192eb5628"
+        self.populateMenu()
+        
+    }
+    
+    func populateMenu(){
+        switch self.displayedMenu
+        {
+        case "Collections":
+            //cal API and get project collections
+            RestApiManager.sharedInstance.getCollections { json in
+                var count: Int = json["count"].int as Int!;         //number of objects within json obj
+                var jsonOBJ = json["value"]                         //get json with projects
                 
-                let id = jsonOBJ[index]["id"].string as String! ?? ""
-                let name: String = jsonOBJ[index]["name"].string as String! ?? ""
-                let url: String = jsonOBJ[index]["url"].string as String! ?? ""
-                
-                let item: (String, String, String) = (id, name, url)
-                self.collections.append(id: id, name: name, url: url)
-                
-                dispatch_async(dispatch_get_main_queue(), {
-                    tableView?.reloadData()})
+                for index in 0...(count-1) {                        //for each obj in jsonOBJ
+                    
+                    let id = jsonOBJ[index]["id"].string as String! ?? ""
+                    let name: String = jsonOBJ[index]["name"].string as String! ?? ""
+                    let url: String = jsonOBJ[index]["url"].string as String! ?? ""
+                    
+                    //                    let item: (String, String, String) = (id, name, url)
+                    self.collections.append(id: id, name: name, url: url)
+                    dispatch_async(dispatch_get_main_queue(), {
+                        tableView?.reloadData()})
+                    
+                }
             }
-//            println(self.collections)
+        case "Teams":
+            RestApiManager.sharedInstance.getTeams { json in
+                var count: Int = json["count"].int as Int!;         //number of objects within json obj
+                var jsonOBJ = json["value"]                         //get json with projects
+                
+                for index in 0...(count-1) {                        //for each obj in jsonOBJ
+                    
+                    let id = jsonOBJ[index]["id"].string as String! ?? ""
+                    let name: String = jsonOBJ[index]["name"].string as String! ?? ""
+                    let url: String = jsonOBJ[index]["url"].string as String! ?? ""
+                    let description: String = jsonOBJ[index]["description"].string as String! ?? ""
+                    let identityUrl: String = jsonOBJ[index]["identityUrl"].string as String! ?? ""
+                    
+                    self.teams.append(id: id, name: name, url: url, description: description, identityUrl: identityUrl)
+                    dispatch_async(dispatch_get_main_queue(), {
+                        tableView?.reloadData()})
+                    
+                }
+            }
+        case "Projects":
+            RestApiManager.sharedInstance.getProjects { json in
+                var count: Int = json["count"].int as Int!;         //number of objects within json obj
+                var jsonOBJ = json["value"]                         //get json with projects
+                
+                for index in 0...(count-1) {                        //for each obj in jsonOBJ
+                    
+                    let id = jsonOBJ[index]["id"].string as String! ?? ""
+                    let name: String = jsonOBJ[index]["name"].string as String! ?? ""
+                    let description: String = jsonOBJ[index]["description"].string as String! ?? ""
+                    let url: String = jsonOBJ[index]["url"].string as String! ?? ""
+                    let state: String = jsonOBJ[index]["state"].string as String! ?? ""
+                    let revision: String = jsonOBJ[index]["revision"].string as String! ?? ""
+                    
+                    self.projects.append(id: id, name: name, description: description, url: url, state: state, revision: revision)
+                    dispatch_async(dispatch_get_main_queue(), {
+                        tableView?.reloadData()})
+                }
+            }
+        default:
+            println(self.displayedMenu)
         }
     }
     
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return self.collections.count;
+        switch self.displayedMenu
+        {
+        case "Collections":
+            return self.collections.count
+        case "Teams":
+            return self.teams.count
+        case "Projects":
+            return self.projects.count
+        default:
+            return 0
+        }
     }
-
+    
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         
         var cell = tableView.dequeueReusableCellWithIdentifier("CELL") as? UITableViewCell
@@ -47,9 +117,30 @@ class menuController: UITableViewController {
             cell = UITableViewCell(style: UITableViewCellStyle.Value1, reuseIdentifier: "CELL")
         }
         
-        cell!.textLabel?.text = self.collections[indexPath.row].name
-
+        var displayedText: String = ""
+        switch self.displayedMenu
+        {
+        case "Collections":
+            displayedText = self.collections[indexPath.row].name
+            cell!.accessoryType = UITableViewCellAccessoryType.DetailButton
+            break;
+        case "Teams":
+            displayedText = self.teams[indexPath.row].name
+            cell!.accessoryType = UITableViewCellAccessoryType.DetailButton
+            break;
+        case "Projects":
+            displayedText = self.projects[indexPath.row].name
+            cell!.accessoryType = UITableViewCellAccessoryType.DetailDisclosureButton
+            break;
+        default:
+            displayedText = "bullshit"
+            
+        }
+        
+        cell!.textLabel?.text = displayedText
         return cell!
+        
+        
     }
     
     override func viewDidLoad() {
@@ -64,9 +155,9 @@ class menuController: UITableViewController {
     
     //sign out button event
     @IBAction func signOutButton(sender: AnyObject) {
-      
+        
         if (KeychainWrapper.hasValueForKey("credentials")){
-                KeychainWrapper.removeObjectForKey("credentials")
+            KeychainWrapper.removeObjectForKey("credentials")
         }
         
         let loginView = self.storyboard!.instantiateViewControllerWithIdentifier("LoginView") as! UINavigationController
