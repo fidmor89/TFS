17243025355b12892440525549dd3e69d34aac6a
Fidel Esteban Morales Cifuentes <fidmor.89@outlook.com>
2015-10-31 14:43:11 -0600

    TaskDetailViewController added and linked to TaskDetailView id on the main storyboard

---
+++ TFS/TaskDetailViewController.swift
@@ -0,0 +1,23 @@
+//
+//  TaskDetailViewController.swift
+//  TFS
+//
+//  Created by Fidel Esteban Morales Cifuentes on 10/31/15.
+//  Copyright (c) 2015 Fidel Esteban Morales Cifuentes. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class TaskDetailViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    
+}
